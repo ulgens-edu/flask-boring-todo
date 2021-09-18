747faf86826530d858c1f1bf31db95fc2f391589
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2021-09-18 23:01:29 +0300

    Create Todo data model

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+from datetime import datetime
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(length=200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Task {self.id}>"
 
 
 @app.route("/")
